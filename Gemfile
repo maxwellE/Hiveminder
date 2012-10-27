0fb06a2a78e42e8831a6ed8a089f4f5fdc723633
Maxwell Elliott <elliott.432@buckeyemail.osu.edu>
2012-10-26 20:13:30 -0400

    ready to start posting comments

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source :rubygems
 gem "pry"
 gem "typhoeus"
 gem 'oj'
+gem 'sequel'
+gem 'sqlite3'
--- src/Hiveminder.rb
+++ src/Hiveminder.rb
@@ -3,10 +3,23 @@ require "bundler/setup"
 require 'pry'
 require 'typhoeus'
 require 'oj'
+require 'sequel'
+require "sqlite3"
 
+
+DB = Sequel.sqlite('hiveminder.db')
 
 module Hiveminder
   def self.grab_comments
+    DB.create_table? :comments do
+      primary_key :id
+      String :comment_id
+      String :comment_text
+      String :response_text
+      Boolean :processed, :default => false
+      Date :posted_response_on
+    end
+    saved_comments = DB[:comments]
     top_posts = Oj.load(Typhoeus::Request.get('http://www.reddit.com/.json').body)
     post_ids = top_posts["data"]["children"].map{|x| x["data"]["id"]}
     post_ids.each do |id|
@@ -14,8 +27,9 @@ module Hiveminder
       text = comments.last["data"]["children"].first["data"]["body"].gsub(/[^\w ]/,"")
       name = comments.last["data"]["children"].first["data"]["name"]
       # next if db contains a processed row with name
-      
-      binding.pry
+      if saved_comments[:comment_id => name].nil?
+          DB[:comments].insert(:comment_id => name, :comment_text => text)
+      end
     end
   end
 end
