bab209e32d34afafa454bf648cbf156f4e25f756
EC2 Default User <ec2-user@ip-10-202-253-154.ec2.internal>
2013-02-11 21:38:07 +0000

    php call fix

--- src/pandorabots.rb
+++ src/pandorabots.rb
@@ -2,7 +2,7 @@ require 'json'
 module Pandorabots
   def get_pandorabots_response(comment_text,bot_id,api_secret,api_key)
     begin
-      response = `php #{File.expand_path("api_request.php")} abc-#{Time.now.to_i.to_s[0..8]} '#{comment_text}' #{api_key} #{api_secret} #{bot_id}`
+      response = `/usr/bin/php /home/ec2-user/Hiveminder/src/api_request.php abc-#{Time.now.to_i.to_s[0..8]} '#{comment_text}' #{api_key} #{api_secret} #{bot_id}`
       response =~ /\)<br>(.+)\z/
       return JSON.load($1)["message"]["message"]
     rescue
