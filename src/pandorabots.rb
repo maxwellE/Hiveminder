cd67d5bfd3e9936b7596a4cb78f8d87b5cbec41b
EC2 Default User <ec2-user@ip-10-202-253-154.ec2.internal>
2013-02-11 21:18:52 +0000

    fixes for ec2

--- src/pandorabots.rb
+++ src/pandorabots.rb
@@ -2,7 +2,7 @@ require 'json'
 module Pandorabots
   def get_pandorabots_response(comment_text,bot_id,api_secret,api_key)
     begin
-      response = `./api_request.php abc-#{Time.now.to_i.to_s[0..8]} '#{comment_text}' #{api_key} #{api_secret} #{bot_id}`
+      response = `php #{File.expand_path("api_request.php")} abc-#{Time.now.to_i.to_s[0..8]} '#{comment_text}' #{api_key} #{api_secret} #{bot_id}`
       response =~ /\)<br>(.+)\z/
       return JSON.load($1)["message"]["message"]
     rescue
