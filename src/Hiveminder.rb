c69aa121b44af38b0120f30ef77256d1a37b4996
EC2 Default User <ec2-user@ip-10-202-253-154.ec2.internal>
2013-02-17 04:40:40 +0000

    updates

--- src/Hiveminder.rb
+++ src/Hiveminder.rb
@@ -12,16 +12,16 @@ module Hiveminder
   extend DbManager
   def self.grab_comments(username, password)
     create_db_and_table?
-    sign_in(username,password)
+    client = sign_in_and_get_client(username,password)
     comment_count = 0
-    get_new_comments.each do |unread_comment|
+    get_new_comments(client).each do |unread_comment|
       unless db_contains_comment?(unread_comment["data"]["name"])
         insert_response_comment(unread_comment["data"]["name"],unread_comment["data"]["body"])
         comment_count +=1
       end
     end
     puts "!!!!   #{comment_count} response comment(s) added.   !!!!"
-    log_out
+    log_out_client(client)
     comment_count = 0
     get_top_posts_ids(5).each do |post_id|
       comment_text,comment_id = grab_top_post_comment(post_id)
@@ -32,24 +32,25 @@ module Hiveminder
       end
     end
     puts "!!!!   #{comment_count} top post comment(s) added.   !!!!"
+    puts "!!!! DONE WITH GRAB !!!!"
+    client.clear_sessions password
   end
 
   def self.post_comments(username,password,api_secret,api_key)
     next_comment = get_next_response_or_post
     if next_comment
-      sign_in(username,password)
       pandorabot_response = next_comment[:response_text] || get_pandorabots_response(next_comment[:comment_text],754,api_secret,api_key)
       if pandorabot_response
         unless next_comment[:response_text]
           save_pandorabot_response(next_comment[:id],pandorabot_response)
         end
-        if perform_comment?(pandorabot_response,next_comment[:comment_id])
+        if perform_comment?(username,password,pandorabot_response,next_comment[:comment_id])
           puts "Responsed to comment #{next_comment[:comment_text]} with #{pandorabot_response}"
           mark_comment_as_processed_in_db(next_comment[:id])
         end
       end
-      log_out
     end
+    puts "DONE WITH POST"
   end
 end
 
--- src/reddit.rb
+++ src/reddit.rb
@@ -3,21 +3,22 @@ require 'oj'
 require 'typhoeus'
 
 module Reddit
-  def sign_in(username,password)
-    @client = Snoo::Client.new
-    @client.log_in username, password
+  def sign_in_and_get_client(username,password)
+    client = Snoo::Client.new
+    client.log_in username, password
+    client
   end
   
-  def log_out
-    @client.log_out
+  def log_out_client(client)
+    client.log_out
   end
   
-  def get_new_comments
-    @client.get_messages("unread",{:mark=>true})["data"]["children"]
+  def get_new_comments(client)
+    client.get_messages("unread",{:mark=>true})["data"]["children"]
   end
   
   def get_top_posts_ids(count) 
-    top_posts = Oj.load(Typhoeus::Request.get('http://www.reddit.com/new/.json').body)
+    top_posts = Oj.load(Typhoeus::Request.get('http://www.reddit.com/.json').body)
     top_posts["data"]["children"].map{|x| x["data"]["id"]}[0...count]
   end
   
@@ -28,11 +29,18 @@ module Reddit
     return text,name
   end
   
-  def perform_comment?(response,comment_id)
+  def perform_comment?(username,password,response,comment_id)
     begin
-      @client.comment(response, comment_id)
+      client = Snoo::Client.new
+      client.log_in username, password
+      client.comment(response, comment_id)
+      client.log_out
+      client.clear_sessions password
       true
     rescue
+      puts "Failed to comment"
+      puts $@
+      puts $!
       false
     end
   end
